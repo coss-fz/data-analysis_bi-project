7945e5e622283b0ec3cf260d917df9c179538e46
coss-fz <julicofito@gmail.com>
2023-07-01 22:22:05 -0500

    unit tests updated

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -12,9 +12,23 @@ import pytest
 
 import pandas as pd
 
-from src.package_module.functions import DataAnalyzer
+from src.package_module.functions import DataAnalyzer, even_numbers_filter
 
 
+
+
+def test_even_numbers_filter():
+    """
+    This function tests the function 'even_numbers_filter'
+    """
+
+    initial_list = [-12,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10,11,12]
+    expected_output = [-12, -10, -8, -6, -4, -2, 2, 4, 6, 8, 10, 12]
+
+    list_even = even_numbers_filter(initial_list)
+    
+    assert isinstance(list_even, list)
+    assert even_numbers_filter(list_even) == expected_output
 
 
 def test_DataAnalyzer():
